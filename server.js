b0825342edfe6dbeeac6f5883c052160a8e82cfa
SantiSnow <aguirresantiago81@gmail.com>
2023-12-23 20:10:48 -0300

    agregado peerjs para manejar conexiones peer to peer

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const { ExpressPeerServer } = require("peer");
+require('dotenv').config();
+
+const app = express();
+const server = app.listen(process.env.SERVER_APP_PORT);
+
+app.get("/", (req, res, next) => {
+    res.send("App running correctly");
+});
+
+const peerServer = ExpressPeerServer(server, {
+	path: "/myapp",
+});
+
+app.use("/peerjs", peerServer);
+
+console.log(`Server running on http://localhost:${process.env.SERVER_APP_PORT}`);
